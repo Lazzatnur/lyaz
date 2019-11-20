a9f087e229e922174b9cd7a0b0e338397c7962b2
Lazzatnur <56934948+Lazzatnur@users.noreply.github.com>
2019-11-21 02:45:29 +0600

    Create 19a.cpp

---
+++ 19a.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{ 
+   int a,b;
+   cout<<"konfet:";
+   cin>>a;
+   cout<<"pechen'e:";
+   cin>>b;
+   cout<<0.3*a+0.4*b;
+}
