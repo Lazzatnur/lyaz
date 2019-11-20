54137cf632f57ea67abefa263fe7a427636bc974
Lazzatnur <56934948+Lazzatnur@users.noreply.github.com>
2019-11-21 02:39:17 +0600

    Create task9.cpp

---
+++ task9.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{ 
+    int x=-2;
+        if(x<0){
+        cout<<(-1)*x+pow(x,5);
+    }else{
+        cout<<x+pow(x,5);
+    }
+    return 0;
+}
