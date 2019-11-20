89d97c28ef3ab34a2ddd70ef7e22acd3218841b8
Lazzatnur <56934948+Lazzatnur@users.noreply.github.com>
2019-11-21 02:53:50 +0600

    Create 30v.cpp

---
+++ 30v.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main ()
+{
+  float a;
+  cin>>a;
+  int b=a;
+     for(int i=0;i<14;i++){
+         a=a*b;
+     }
+      cout<<a;
+  return 0;
+}
