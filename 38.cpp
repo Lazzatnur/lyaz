b6a8c9a0a79aec153c6abbf6613dc113e46a7f62
Lazzatnur <56934948+Lazzatnur@users.noreply.github.com>
2019-11-21 02:58:17 +0600

    Create 38.cpp

---
+++ 38.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+   if (m-n==100 || m-n==-100)
+       cout<<"yes";
+   else
+    cout<<"no";
+}
