b2a45ead58fc25f1aa518158775a65ba5f3acf72
Lazzatnur <56934948+Lazzatnur@users.noreply.github.com>
2019-11-21 03:02:55 +0600

    Create 48.cpp

---
+++ 48.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    int a,b,c,max;
+    cin >>a>>b>>c;
+    if (a+b>max) max = a+b;
+    if (a+c>max) max = a+c;
+    if (c+b>max) max = c+b;
+    if (max==a+b)cout << a<<" "<<b;
+    if (max==a+c)cout << a<<" "<<c;
+    if (max==c+b)cout << c<<" "<<b;
+}
