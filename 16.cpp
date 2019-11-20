a42cbf86ec0b7d431a753c557d9be44e527d1968
Lazzatnur <56934948+Lazzatnur@users.noreply.github.com>
2019-11-21 02:43:48 +0600

    Create 16.cpp

---
+++ 16.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{ 
+   int a,b,c;
+   cin>>a>>b>>c;
+   a=3*a;
+   b=b-3;
+   c=c*c;
+   cout<<a<<endl<<b<<endl<<c<<endl<<a+b+c;
+}
