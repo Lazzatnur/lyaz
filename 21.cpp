44e42f4e42575f80470c7feab93243f6d36a0908
Lazzatnur <56934948+Lazzatnur@users.noreply.github.com>
2019-11-21 02:46:56 +0600

    Create 21.cpp

---
+++ 21.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main ()
+{
+  float a, b, c;
+  cin>>a>>b;
+  c = sqrt (a * a + b * b);
+float p=a+b+c;
+float s= a*b;
+cout<<p<<endl<<s<<endl<<c;
+  return 0;
+}
